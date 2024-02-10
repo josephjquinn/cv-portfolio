aa0021c58d13cc83bc5774eb6afe711cff99e409
josephjquinn <jquinn2005@outlook.com>
2024-02-09 19:17:10 -0600

    updating packages / integration

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,23 @@ import './App.css';
 import {Navbar} from "./components/Navbar/Navbar.tsx"
 import {Home} from "./components/Home/Home.tsx"
 import {About} from "./components/About/About.tsx"
+import {Projects} from "./components/Projects/Projects.tsx"
+import {Skills} from "./components/Skills/Skills.tsx"
+
+
 
 
 import React from "react";
+import Contact from "./components/Contact/Contact.tsx";
 const App: React.FC = () => {
     return (
         <div className="App">
             <Navbar/>
             <Home/>
             <About/>
+            <Projects/>
+            <Skills/>
+            <Contact/>
         </div>
     );
 }
