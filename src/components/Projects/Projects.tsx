6c729ba96f00399763eb58d640ab1dbe48d7da28
josephjquinn <jquinn2005@outlook.com>
2024-02-11 01:18:42 -0600

    updating project descriptions and subtitles

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -33,47 +33,61 @@ const projectArr: ProjectData[] = [
         img: wordwise,
         title: "Wordwise",
         img_des: wordwise_des,
-        subtitle: "Control character in 2D game.",
+        subtitle: "Wordle Solving Algorithm",
         description: (
-            <p>In this project, I use my advantage skill to design my own character. It’s inspired by the game Hollow Knight. Then I create a sprite sheet with run, attack, jump, and dash actions. Finally, make animation by using Javascript.
+            <p>This project is a Python implementation of a Wordle solver using the Selenium web automation framework.
+                Wordle is a word puzzle game where the player tries to guess a five-letter word within a limited number
+                of attempts.
                 <br/><br/>
-                Thanks to the powerful HTML Canvas Graphic Method that makes it is easy for me to render each frame correctly. With the retention of the image in the human eye, if I render each frame of an action in a short time, I can create the feeling that the character is moving. Similar to the old-generation 2D games.
+                The algorithm at the core of this project is designed to systematically approach solving Wordle puzzles
+                efficiently. It utilizes feedback from previous guesses, analyzing which letters are correctly guessed
+                but misplaced, which are correctly guessed and correctly positioned, and which are incorrect.
+                Through a process of elimination and statistical analysis of letter frequencies, the algorithm refines
+                the pool of potential words and selects the optimal guess based on calculated scores,
+                increasing the likelihood of correctly guessing the target word within the allotted attempts.
                 <br/><br/>
-                In addition to managing a large number of character properties, I have to use "Object" and its methods. This project helped me understand a lot more about Object Oriented.
-                <br/><br/>
-                Note: All of these things, I learned from the Franks Laboratory.
+                See Github for more information and detailed documentation
             </p>
         ),
-        github: "https://github.com/thekietvuong/Sprite-Animation-in-JavaScript"
+        github: "https://github.com/josephjquinn/word-wise"
     },
     {
         img: snakedql,
         title: "Snake Deep Learning",
         img_des: snakedql_des,
-        subtitle: "The RedFox website",
+        subtitle: "DQL Model",
         description: (
-            <p>This project is a website about red foxes that includes interesting information about their characteristics, habitat, food, and reproduction,...
+            <p>This repository contains a Python implementation of a Deep Q-Learning model that plays a basic Snake
+                arcade game.
                 <br/><br/>
-                Talking about the technical aspects, this is a simple website consisting of just a navbar with an animation that spans out when the user hovers over it. Along with eye-catching page transition effects.
-                <br/><br/>
-                To do that, I had to combine React Router Dom and Framer motion.
-                <br/><br/>
-                Let's click on my Youtube video links below to find out in detail how I designed the logo, and how I coded the website from scratch.
+                Deep Q-Learning is a popular reinforcement learning algorithm that allows an agent to learn optimal
+                strategies to maximize its cumulative reward in a given environment.<br/>
+                <br/>
+                This system relies on a reward system in order to train the model, every action the snake takes has the
+                potential for a change in the reward score, the snake wants to increase its reward at all times and the
+                model trains itself to be the most effect at this task.
+                <br/>
+                <br/>
+                See Github for more information and detailed documentation
             </p>
         ),
-        github: "https://github.com/thekietvuong/The-Red-Fox-Information-Website"
+        github: "https://github.com/josephjquinn/Huffman-Encoding-Algorithm"
     },
     {
         img: huffman,
         title: "Huffman Encoding",
         img_des: huffman_des,
-        subtitle: "POKEMON API",
+        subtitle: "Compression Algorithm",
         description: (
-            <p>This project is a front-end developer test of the MOCK interview. Fetching Pokemon API with React as a framework. Once again, I took advantage of my graphic design to create a simple yet interesting UI.
+            <p>
+                This Java program implements the Huffman encoding algorithm for lossless data compression.
                 <br/><br/>
-                This project gives me the opportunity to work with fetching data, array method, filtering, and finding the corresponding pokemon when the user types in the search bar.
+                It analyzes the frequency of characters in a user-input string, constructs a Huffman tree prioritizing characters by frequency, and assigns variable-length codes to each character.
                 <br/><br/>
-                It will be interesting and I'm sure you don't want to miss my video below. Let's check it out
+                The program outputs the binary representation of the encoded string and logs the Huffman codes for each character to a text file, facilitating efficient data storage and transmission.
+                <br/>
+                <br/>
+                See Github for more information and detailed documentation
             </p>
         ),
     },
