d5581187b8c5649e334ecde21472e72b14ae96e5
josephjquinn <jquinn2005@outlook.com>
2024-02-11 15:58:37 -0600

    updating logo

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from "react"
 import "./home.css"
 
-import logo from "../../assets/logo.png"
+import logo from "../../assets/logo2.png"
 
 import gsap from "gsap";
 import {ScrollTrigger} from "gsap/ScrollTrigger";
